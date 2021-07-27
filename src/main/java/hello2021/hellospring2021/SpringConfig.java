8daec4066abdc909e1b7463a3b29fac00e37d21e
lim <limyoonji93@gmail.com>
2021-07-27 22:06:42 +0900

    jdbc연결

--- src/main/java/hello2021/hellospring2021/SpringConfig.java
+++ src/main/java/hello2021/hellospring2021/SpringConfig.java
@@ -1,20 +1,32 @@
 package hello2021.hellospring2021;
 
+import hello2021.hellospring2021.repository.JdbcMemberRepository;
 import hello2021.hellospring2021.repository.MemberRepository;
-import hello2021.hellospring2021.repository.MemoryMemberRepository;
 import hello2021.hellospring2021.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-    @Bean
-    public MemberService memberService() {
-    return new MemberService(memberRepository());
+
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+        //        return new MemoryMemberRepository();
+
     }
 }
